65f81c8eb762b8d8d4ecc500199808992fe461b7
Luke <lukeconrad.stew@gmail.com>
2021-12-06 16:20:17 -0800

    added color to time slots

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,39 @@
-console.log ("welcome");
+console.log("welcome");
 
 // Varibles
 
 var today = $("#currentDay")
+var now = moment().format("dddd, MMM Do YYYY [at] h:mm:ss a");
+$('#currentDay').text(now);
 
 // live clock telling date & time
-setInterval(function() {
-     var now = moment().format("dddd, MMM Do YYYY [at] h:mm:ss a");
+setInterval(function () {
+    var now = moment().format("dddd, MMM Do YYYY [at] h:mm:ss a");
     $('#currentDay').text(now);
-    }, 1000);
+}, 1000);
+
+function color() {
+    $("textarea").each(function () {
+        var time = $(this).data("time")
+        var hour = moment().hour()
+        console.log(time, hour)
+        if (time === hour) {
+            $(this).addClass("present")
+        }
+        else if (time < hour) {
+            $(this).addClass("future") 
+            } 
+        else {
+            $("past")
+        }
+    })
+                
+            
+
+
+
+}
+color();
+
+
 
