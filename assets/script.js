98937a8a06078e982f20d6bb4d2e6c5a1f8174f9
Lucas Stewart <89881129+L-Stew206@users.noreply.github.com>
2021-12-06 23:38:17 -0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -5,12 +5,45 @@ console.log("welcome");
 var today = $("#currentDay")
 var now = moment().format("dddd, MMM Do YYYY [at] h:mm:ss a");
 $('#currentDay').text(now);
+var plans;
+var storedPlans = localStorage.getItem("plans");
 
-// live clock telling date & time
+// Turning on saved button response
+
+$(".saveBtn").on('click', savePlans);
+
+//  Checking if anyting was entered into times, if not, nothing gets saved. Anyting else will be saved
+
+function initialPage() {
+    if (!storedPlans) {
+        plans = {};
+    } else {
+        plans = JSON.parse(storedPlans);
+        console.log(plans);
+    }
+}
+
+//  Storing plans into the storage
+
+function savePlans(event) {
+    var content = $(event.target);
+    var test = content.attr("id");
+    var savedPlan = content.parent().siblings("textarea").val();
+    plans[test] = savedPlan;
+    localStorage.setItem("plans", JSON.stringify(savedPlan));
+    console.log(savedPlan);
+}
+
+
+
+// Live clock telling date & time
+
 setInterval(function () {
     var now = moment().format("dddd, MMM Do YYYY [at] h:mm:ss a");
     $('#currentDay').text(now);
 }, 1000);
+
+// Gives color to time slots dependind on the current hours status
 
 function color() {
     $("textarea").each(function () {
@@ -20,20 +53,26 @@ function color() {
         if (time === hour) {
             $(this).addClass("present")
         }
-        else if (time < hour) {
-            $(this).addClass("future") 
-            } 
+        else if (time > hour) {
+            $(this).addClass("future")
+        }
         else {
             $("past")
         }
     })
-                
+}
+
+
+
+// Generates functions 
+
+color();
+initialPage();
             
 
 
 
-}
-color();
+
 
 
 
