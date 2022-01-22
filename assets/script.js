337b9ee6eb429d4f25d282640f80ce34d3e4b92d
Luke <lukeconrad.stew@gmail.com>
2022-01-21 18:15:32 -0800

    got local storage to work by using a for loop: had to give each button an individual id to work: added a substring method to remove the quotes

--- assets/script.js
+++ assets/script.js
@@ -1,52 +1,58 @@
-console.log("wtf");
-
 // Varibles
-
 var today = $("#currentDay")
 var now = moment().format("dddd, MMM Do YYYY [at] h:mm:ss a");
 $('#currentDay').text(now);
 var plans = []
 
 // Turning on saved button response
-
 $(".saveBtn").on('click', savePlans);
 
 //  Checking if anyting was entered into times, if not, nothing gets saved. Anyting else will be saved
+function loadPage() {
+    // console.log(localstorage)
+    // loop over all the elements in localStorage and set the element previous to buttonID (key) to the value
+    for (var i = 0; i < localStorage.length; i++) {
+        key = localStorage.key(i);
+        value = localStorage.getItem(key);
+        console.log (key + " " + value)
 
-function initialPage() {
-    var storedPlans = localStorage.getItem("plans");
-    if (!storedPlans) {
-        plans = [];
-    } else {
-        plans = JSON.parse(storedPlans);
-        console.log(plans);
+        // TODO: use the key to find the  element (previous) and set the value 
+        // get buttonID
+        buttonID = key;
+        //derive textareaid from buttonid
+        textAreaID = buttonID.substring(0, buttonID.length - 1);
+        // clean up the value by chopping off the quote from the front and the back
+        unquotedValue = value.substring(1, value.length-1)
+        //finally set the value to the script element
+        document.getElementById(textAreaID).value = unquotedValue;
     }
 }
 
 //  Storing plans into the storage
-
 function savePlans(event) {
     var content = $(event.target);
-    console.log(content)
-    var test = content.attr("id");
-    console.log(test)
-    var savedPlan = content.parent().siblings("textarea").val();
-    plans[test] = savedPlan;
-    localStorage.setItem("plans", JSON.stringify(savedPlan));
-    console.log(savedPlan);
+    console.log(content);
+    var buttonID = content.attr("id");
+    console.log(buttonID);
+    console.log(content.prev().val());
+    var saveText = content.prev().val();
+    localStorage.setItem(buttonID, JSON.stringify(saveText));
 }
-
-
-
+    
+    // var savedPlan = content.parent().siblings("textarea").val();
+    // plans[test] = savedPlan;
+    // localStorage.setItem("plans", JSON.stringify(savedPlan));
+    // console.log(savedPlan);
+   //^^ didnt work, 
+    
 // Live clock telling date & time
-
 setInterval(function () {
     var now = moment().format("dddd, MMM Do YYYY [at] h:mm:ss a");
     $('#currentDay').text(now);
 }, 1000);
 
-// Gives color to time slots dependind on the current hours status
 
+// Gives color to time slots dependind on the current hours status
 function color() {
     $("textarea").each(function () {
         var time = $(this).data("time")
@@ -64,15 +70,9 @@ function color() {
     })
 }
 
-
-
 // Generates functions 
-
 color();
-initialPage();
-            
-
-
+loadPage();
 
 
 
